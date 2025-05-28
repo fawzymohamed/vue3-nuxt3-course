9d90d4d512caa01a8ea8043a03e67196fd8c3f2c
Fawzy Mohamed <fmohamed@alkholi.com>
2025-05-28 16:03:11 +0300

    Nuxt UI integration done

--- app.vue
+++ app.vue
@@ -1,5 +1,12 @@
 <template>
-  <div>
-    <h1>Hello World</h1>
-  </div>
+  <UApp>
+    <div class="container mx-auto p-4">
+      <div>
+        <UCard color="primary" class="text-amber-800 shadow-2xl">
+          <h3 class="text-lg font-medium mb-2">Colored Card</h3>
+          <p>This card has a primary background color</p>
+        </UCard>
+      </div>
+    </div>
+  </UApp>
 </template>
