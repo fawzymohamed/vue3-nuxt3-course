6e43b73bd8bf587c7c89d3995674c055e3d7d8f5
Fawzy Mohamed <fmohamed@alkholi.com>
2025-06-01 17:13:33 +0300

    configure content module initially

--- pages/index.vue
+++ pages/index.vue
@@ -5,17 +5,30 @@ const switchLocalePath = useSwitchLocalePath();
 const availableLocales = computed(() => {
   return locales.value.filter((i) => i.code !== locale.value);
 });
+
+const { data: home } = await useAsyncData(() =>
+  queryCollection("content").path("/").first()
+);
 </script>
 
 <template>
-  <div>
-    <NuxtLink
-      v-for="locale in availableLocales"
-      :key="locale.code"
-      :to="switchLocalePath(locale.code)"
-    >
-      {{ locale.name }}
-    </NuxtLink>
-    <h1>{{ $t("welcome") }}</h1>
-  </div>
+  <UContainer>
+    <!-- Content Section -->
+    <div>
+      <ContentRenderer v-if="home" :value="home" />
+    </div>
+
+    <!-- Language Switch Links -->
+    <div class="py-10">
+      <NuxtLink
+        v-for="loc in availableLocales"
+        :key="loc.code"
+        :to="switchLocalePath(loc.code)"
+        class="pb-85"
+      >
+        {{ loc.name }}
+      </NuxtLink>
+      <h1 class="py-5 text-3xl">{{ $t("welcome") }}</h1>
+    </div>
+  </UContainer>
 </template>
